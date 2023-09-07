2cf9b87f2adc605cf2c3510d58b66cf3ec741df6
ajwebdev <ganeshajith980@gmail.com>
2023-09-07 16:08:22 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as _ from 'lodash';
 import './style.css';
 
 export default function App() {
+  const [triCount, setTriCount] = useState(0);
+  const [clickedCount, setClickedCount] = useState(0);
+  const debouncedCount = _.debounce(() => {
+    setTriCount(clickedCount + 1);
+  }, 1000);
+
+  const handleClick = () => {
+    setClickedCount(1 + clickedCount);
+    debouncedCount();
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1> Clicked count {clickedCount}</h1>
+      <h1>Triggered count {triCount}</h1>
+
+      <button onClick={handleClick}> clickedcount count</button>
+      <br />
+      <br />
     </div>
   );
 }
